927c7de09b0794f3c378e8583494770d4dce5335
Ege Gürkan <93516630+EgeGurkan@users.noreply.github.com>
2026-09-17 11:34:26 +0200

    Add files via upload

--- ByteUpdater.cmd
+++ ByteUpdater.cmd
@@ -1,5 +1,5 @@
 :: Starting Parameters
-@echo off & bgcolor(a) & chcp 1254 & mode con: cols=55 lines=9 & title Byte Updater & cls
+@echo off & chcp 1254 & mode con: cols=55 lines=9 & title Byte Updater & cls
 
 :: Run As Administrator
 >nul reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\" &call \"%%2\" %%3" &set _= %*
